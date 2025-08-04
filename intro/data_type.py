9272a940d2fd7212626321b47ea5d2b3aa73fa30
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-04 16:13:24 +0300

    how to print

--- intro/data_type.py
+++ intro/data_type.py
@@ -1,13 +1,41 @@
 # snake case
 # snake_case
-#indentation
+# indentation
 
 x=10 #Integer 
+y=x # This ius a new variable
+print(x)
+print(type(x))
 pi=3.142  #Floating
+print(pi)
+print(type(pi))
 myName="Daniel" #String
+print(myName)
+print(type(myName))
 myName1='Daniel' #String
+print(myName1)
+print(type(myName1))
 is_happy=True # python True/False js true/false
+print(is_happy)
+print(type(is_happy))
 no_value=None # None is the equivalent to Null in js
+print(no_value)
+print(type(no_value))
 
-#Advanced Datatypes 
-my_array=[10,"string",True,None,123,[]] # List<Array>
+# Advanced Datatypes Bracket notation
+my_list=[10,10,"string",True,None,123,[]] # List<Array> my_list[0]
+print(my_list)
+print("type of my list",type(my_list))
+print(my_list[5])
+my_set={1,1,1,1,1,1,2,3,3,"Hello","Hello"} #Set(unique stuff only) and Array
+print(my_set)
+print(type(my_set))
+my_tuple=(1,3,4,5,"Apple") #tuple my_tuple[0]=Apple
+print(my_tuple)
+print("my_tuple",type(my_tuple))
+my_obj={"key":"value","age":23} #Dictionary Key<->value my_obj["key"].
+print(my_obj)
+# Print ALL Value: python print() >---< js<console.log> 
+
+#PASS BY REF Mutable , PASS BY VALUE-->IMUATABLE
+#Array from a list is the mutability
