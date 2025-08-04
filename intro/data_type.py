0b7f0e30cad2c0c91769b7c67639ff24397b76f6
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-04 15:32:57 +0300

    python intro

---
+++ intro/data_type.py
@@ -0,0 +1,13 @@
+# snake case
+# snake_case
+#indentation
+
+x=10 #Integer 
+pi=3.142  #Floating
+myName="Daniel" #String
+myName1='Daniel' #String
+is_happy=True # python True/False js true/false
+no_value=None # None is the equivalent to Null in js
+
+#Advanced Datatypes 
+my_array=[10,"string",True,None,123,[]] # List<Array>
