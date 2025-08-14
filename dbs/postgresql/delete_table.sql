3d9028fdecfeebcb001eff4ffc433a260badb11b
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-14 21:13:10 +0300

    create and delete tables

---
+++ dbs/postgresql/delete_table.sql
@@ -0,0 +1,1 @@
+DROP TABLE mwanafunzi,mzazi,parent,student
---
+++ dbs/sqlite/create_tables.sql
@@ -0,0 +1,8 @@
+
+--CREATE TABLE mwanafunzi(
+--    id SERIAL PRIMARY KEY
+--);
+
+--CREATE TABLE mzazi(
+--    id SERIAL PRIMARY KEY
+--);
