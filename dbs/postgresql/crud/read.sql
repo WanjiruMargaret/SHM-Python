f384aa7f16db247502c8e599ee9c50d1eb37e774
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-18 11:09:17 +0300

    aad read

---
+++ dbs/postgresql/crud/read.sql
@@ -0,0 +1,26 @@
+--SELECT <COLUMNS \\ *>
+--FROM <table>
+--WHERE <READ CONSTRAINTS>
+
+
+
+--SELECT * from student
+--WHERE email = 'sam@gmail.com'
+--OR
+--email='alexnganga@gmail.com
+
+--SELECT * from student
+--WHERE pocket_money>50
+--AND pocket_money<100
+
+SELECT * from student
+ORDER BY name ASC
+LIMIT
+
+
+
+
+
+
+
+
