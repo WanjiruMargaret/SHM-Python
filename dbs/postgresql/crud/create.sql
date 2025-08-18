3f10d3883472970b4b6c27dc52c5f58c51161982
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-18 10:16:36 +0300

    add changes

---
+++ dbs/postgresql/crud/create.sql
@@ -0,0 +1,7 @@
+--INSERTINTO <table_name>
+--(column) VALUES (), () ,()
+
+INSERT INTO  student
+(name,email,phone)
+VALUES
+('John Doe','johndoe@gmail.com',25467480),
---
+++ dbs/sqlite/constraint.sql
@@ -0,0 +1,6 @@
+--Contain your data.
+--To ensure is ok <make>
+--an email string unique
+--phone numbers are unique: <>
+--Rules data intergity
+ 
