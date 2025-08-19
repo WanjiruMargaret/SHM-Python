808428b8ccb35509aa2656dd5f1116eab142299f
WanjiruMargaret <maggiesamwells@gmail.com>
2025-08-19 09:29:11 +0300

    add delete and update

---
+++ dbs/postgresql/crud/update.sql
@@ -0,0 +1,14 @@
+--Update <table_name>
+--SET <columns to >
+--where <constraint>
+
+UPDATE student
+SET 
+markS=0
+WHERE markS IS NULL
+
+SELECT name,marks
+FROM student
+
+UPDATE email ='john@john.com'
+WHERE email = 'johndoe@gmail.com'
